22ea408f50fb21e1bed871db3910d92fb8f24eba
seniorstack007 <seniorstack007@gmail.com>
2023-08-23 21:37:07 +0200

    add build

---
+++ build/static/js/1.95377676.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[1],{476:function(a,s,d){}}]);
+//# sourceMappingURL=1.95377676.chunk.js.map
