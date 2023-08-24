d75e7c283c07230ee8cd9abde66bdfa779ca8e9e
seniorstack007 <seniorstack007@gmail.com>
2023-08-24 02:47:27 +0200

    build23

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
-// ----------------------------------------------------------------------
-const userData = JSON.parse(localStorage.getItem('userData'));
-if (userData) {
-    const accessToken = userData.accessToken;
-    axios.defaults.headers.common = {
-        Authorization: `Bearer ${accessToken.replace(/"/g, '')}`
-    };
-}
-
 const axiosInstance = axios.create({baseURL: process.env.REACT_APP_SERVER_URL})
+
+axiosInstance.interceptors.request.use(
+    function (config) {
+        const userData = JSON.parse(localStorage.getItem('userData'));
+        if (userData) {
+            const accessToken = userData.accessToken;
+            config.headers.Authorization = `Bearer ${accessToken.replace(/"/g, '')}`;
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
